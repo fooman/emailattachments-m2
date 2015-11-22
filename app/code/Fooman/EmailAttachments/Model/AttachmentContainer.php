deba256b69d9c1ead396e937ea320fcd2b4c7727
Kristof Ringleff <kristof@fooman.co.nz>
2015-11-23 00:08:04 +1300

    add licensing info

--- app/code/Fooman/EmailAttachments/Model/AttachmentContainer.php
+++ app/code/Fooman/EmailAttachments/Model/AttachmentContainer.php
@@ -1,5 +1,12 @@
 <?php
-
+/**
+ * @author     Kristof Ringleff
+ * @package    Fooman_EmailAttachments
+ * @copyright  Copyright (c) 2015 Fooman Limited (http://www.fooman.co.nz)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 namespace Fooman\EmailAttachments\Model;
 
 class AttachmentContainer implements Api\AttachmentContainerInterface
--- app/code/Fooman/EmailAttachments/Model/Email/Sender/CreditmemoCommentSender.php
+++ app/code/Fooman/EmailAttachments/Model/Email/Sender/CreditmemoCommentSender.php
@@ -1,5 +1,12 @@
 <?php
-
+/**
+ * @author     Kristof Ringleff
+ * @package    Fooman_EmailAttachments
+ * @copyright  Copyright (c) 2015 Fooman Limited (http://www.fooman.co.nz)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 namespace Fooman\EmailAttachments\Model\Email\Sender;
 
 class CreditmemoCommentSender extends \Magento\Sales\Model\Order\Email\Sender\CreditmemoCommentSender
