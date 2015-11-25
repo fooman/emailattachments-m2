83adc0dd9402d718dbe35fe4040aa5b2494e76f3
Kristof Ringleff <kristof@fooman.co.nz>
2015-11-26 00:47:55 +1300

    add extra module check against Print Order Pdf

--- app/code/Fooman/EmailAttachments/Observer/AbstractSendOrderObserver.php
+++ app/code/Fooman/EmailAttachments/Observer/AbstractSendOrderObserver.php
@@ -14,6 +14,19 @@ class AbstractSendOrderObserver extends AbstractObserver
     const XML_PATH_ATTACH_PDF = 'sales_email/order/attachpdf';
     const XML_PATH_ATTACH_AGREEMENT = 'sales_email/order/attachagreement';
 
+    protected $moduleManager;
+
+    public function __construct(
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
+        \Fooman\EmailAttachments\Model\AttachmentFactory $attachmentFactory,
+        \Fooman\EmailAttachments\Model\Api\PdfRendererInterface $pdfRenderer,
+        \Magento\CheckoutAgreements\Model\ResourceModel\Agreement\CollectionFactory $termsCollection,
+        \Magento\Framework\Module\Manager $moduleManager
+    ) {
+        parent::__construct($scopeConfig, $attachmentFactory, $pdfRenderer, $termsCollection);
+        $this->moduleManager = $moduleManager;
+    }
+
     public function execute(\Magento\Framework\Event\Observer $observer)
     {
 
@@ -21,11 +34,12 @@ class AbstractSendOrderObserver extends AbstractObserver
          * @var $order \Magento\Sales\Api\Data\OrderInterface
          */
         $order = $observer->getOrder();
-        if ($this->scopeConfig->getValue(
-            static::XML_PATH_ATTACH_PDF,
-            \Magento\Store\Model\ScopeInterface::SCOPE_STORE,
-            $order->getStoreId()
-        )
+        if ($this->moduleManager->isEnabled('Fooman_PrintOrderPdf')
+            && $this->scopeConfig->getValue(
+                static::XML_PATH_ATTACH_PDF,
+                \Magento\Store\Model\ScopeInterface::SCOPE_STORE,
+                $order->getStoreId()
+            )
         ) {
             $this->attachPdf(
                 $this->pdfRenderer->getPdfAsString([$order]),
